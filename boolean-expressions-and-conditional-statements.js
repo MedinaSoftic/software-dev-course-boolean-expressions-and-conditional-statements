b097d4d364decfad3e2d73e7491970ee8396dde3
Medina Softic <medinasoftic@Mac.attlocal.net>
2025-03-14 00:40:30 -0500

    inital commit

--- boolean-expressions-and-conditional-statements.js
+++ boolean-expressions-and-conditional-statements.js
@@ -26,21 +26,67 @@ Paste the following code into your editor:
 
 const readline = require('readline-sync');
 
-const hasTorch = true;
-const hasMap = false;
+let hasTorch = true;
+let hasMap = false;
+let hasSword = true;
+const hasArmor = false;
+const hasCompass = true;
 
+let playerName = readline.question("Welcome traveler, What is your name? ");
+console.log(playerName + ", Seems you have passed out. You may have some tools in your satchel.");
 console.log("You see two paths: one leads to the mountains, the other to the village.");
-const choice = readline.question("Do you go to the 'mountains' or the 'village'?");
+let choice = readline.question("Do you go to the 'mountains' or the 'village'? ").toLowerCase();
 
-if (choice === "mountains" && hasTorch) {
-  console.log("You safely navigate through the dark mountains.");
-} else if (choice === "mountains" && !hasTorch) {
-  console.log("It's too dark to proceed. You decide to turn back.");
-} else if (choice === "village" || hasMap) {
-  console.log("You find your way to the village.");
-} else {
-  console.log("You get lost and wander aimlessly.");
+//Moutain Path
+if (choice === 'mountains') {
+    if (hasTorch) {
+    console.log("You safely navigate through the dark mountains. You hear footsteps follow. Do you attack or let them approach? ");
+    const mountainStranger = readline.question("'attack' or 'approach'? Please choose: ").toLowerCase();
+    if (mountainStranger === 'attack' && (hasSword || hasArmor)) {
+      console.log("You have attacked the stranger, They were a friend now turned enemy! Get ready to fight!\nThe stranger swings a sword at your arm! Luckily you state 'Tis but a scratch! The stranger swings the swordoun at the other arm!\nNow you have lost both arms!\nThe stranger tells you you've lost. You then say 'Tis but a flesh wound!'\nYOU HAVE LOST YOUR SWORD AND TORCH!\nYou ran away.");
+      //Player loses items
+      hasSword = false;
+      hasTorch = false;
+
+      choice = readline.question("Do you keep going in the 'mountains' or try to find the 'village'? ").toLowerCase();
+    } else if (mountainStranger === "approach") {
+      console.log("It\'s your friend! They will guide you to safety! You have WON!");
+    }
+  }
 }
+if (!hasTorch) {
+  console.log("It's too dark to proceed. Lets find a torch!");
+  const findTorch = readline.question("Do you look for the torch? ' Yes or No '").toLowerCase();
+  if (findTorch === 'yes') {
+    console.log("You look around and see an old torch! You light it up! You also found a Map!");
+    hasTorch = true;
+    hasMap = true;
+  } else if (findTorch === 'no') {
+    console.log(playerName + ", You have decided to stay in the dark...");
+  }
+}
+    if (hasTorch && hasMap) {
+    const goToVillage = readline.question("Should we go to the village? Yes or No? ").toLowerCase();
+    if (goToVillage === 'yes') {
+      choice = 'village';
+    } else if (goToVillage === "no") {
+      console.log(playerName + ", You have decided not to go...your fate has been sealed.");
+    }
+  }
+//Village Path
+if (choice === 'village' && hasTorch && hasMap) {
+  console.log("You find your way to the village. " + playerName + ", You see the tavern. Let's get a drink?");
+  const letsDrink = readline.question(" Yes or No? ").toLowerCase()
+    if (letsDrink === 'yes') {
+  console.log("After " + playerName + " enters the tavern, they drink all night! " + playerName + " wakes up so drunk that they notice the locals have robbed them blind.\n YOU LOST! Try again!");
+    } else if (letsDrink === 'no') {
+      console.log("Instead " + playerName + " goes back home and is able to see another day.\nGet some rest traveler. Welcome home!\nYOU WIN!");
+    }
+  } else if (choice === 'village' && hasSword) {
+    console.log("You get lost and wander aimlessly till you fall ill of exhaustion. ");
+}
+console.log("Till Next time " + playerName + ".");
+
 
 /* 
 
